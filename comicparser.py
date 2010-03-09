3308ece59c355d9f9cbd38bab81b89f3436a1830
Chester <cd@pobox.com>
2010-03-09 09:14:14 -0300

    simplified the html to a single-step parsing

--- comicparser.py
+++ comicparser.py
@@ -5,25 +5,19 @@ from lxml import etree
 from lxml import html
 
 def findLinks(source, target):
-	""" Returns a list of tuples with the xpath and content of every link from source URL
-		to target URL"""
+	""" Locates all links from the source URL to the target URL, returning a list 
+		of 2-element tuples with each link's xpath and inner HTML """
 	links = []
 	source_socket = urllib.urlopen(source)
-	fixed_tree = etree.parse(source_socket, etree.HTMLParser())
-	fixed_html = html.tostring(fixed_tree.getroot(), pretty_print=True, method="xml")
-	absolute_document = html.document_fromstring(fixed_html, base_url=source)
-	absolute_document.make_links_absolute()
-	absolute_html = html.tostring(absolute_document, pretty_print=True, method="xml")
-#	fixed_html = html.tostring(source_html.parse().getroot(),pretty_print=True, method="html")
-#	print absolute_html
-	context = etree.iterparse(StringIO(absolute_html), tag="a")
+	absolute_tree = html.document_fromstring(source_socket.read(), base_url=source)
+	absolute_tree.make_links_absolute()
+	source_socket.close()
+	context = etree.iterwalk(absolute_tree, tag="a")
 	for action, elem in context:
-#		print(etree.tostring(elem))
 		if "href" in elem.attrib:
 			url = elem.attrib["href"]
 			if url == target:
 				links.append((etree.ElementTree(elem).getpath(elem), elem.text))
-	source_socket.close()
 	return links
 
 def getNext(source, xpath, expected_html):
