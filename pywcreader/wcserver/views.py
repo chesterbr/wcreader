fc6fb4b56c97e06e7bc1e63aad23604584d5b974
Chester <cd@pobox.com>
2010-04-27 00:06:43 -0300

    HTTP Basic authentication working

--- pywcreader/wcserver/views.py
+++ pywcreader/wcserver/views.py
@@ -6,10 +6,12 @@ from django.contrib.auth.models import User
 import re
 from pywcreader.wcserver import api_facade
 from models import Comic, Episode
+import time
+import base64
 
 def dispatch(request):
     """Translates the request into one of the operations defined below"""
-    
+
     # Get the components from URL (with related objects), and body parameters 
     path = request.path + ("/" if request.path[-1] != "/" else "")
     mo_user = re.search(r"/user_(.+?)/", path)
@@ -32,21 +34,21 @@ def dispatch(request):
         episodes = Episode.objects.filter(id=mo_episode.group(1))
         if episodes:
             episode = episodes[0]
-    params = QueryDict(request.raw_post_data, encoding=request.encoding)
-    
-    # Dispath
+    params = QueryDict(request.raw_post_data, encoding=request.encoding)    
+        
+    # Dispatch
     if re.match(r"^/comics/?$", path) and request.method == "GET":
         return api_facade.listAllComics();
-    if re.match(r"^/user_"+username+"/$", path) and request.method == "PUT":
-        return user_exists(user) or api_facade.createUser(username, params)
-    if re.match(r"^/user_"+username+"/comic_[0-9]+?/read_episodes/$", path) and request.method == "GET":
-        return user_missing(user) or comic_missing(comic) or api_facade.listReadEpisodes(user, comic)
-    if re.match(r"^/user_.+?/episode_[0-9]+?/$", path) and request.method == "PUT":
-        return user_missing(user) or episode_missing(episode) or api_facade.readEpisode(user, episode)
-    if re.match(r"^/user_.+?/episode_[0-9]+?/$", path) and request.method == "DELETE":
-        return user_missing(user) or episode_missing(episode) or api_facade.unreadEpisode(user, episode)
     if re.match(r"^/comic_[0-9]+?/episodes/$", path) and request.method == "GET":
         return comic_missing(comic) or api_facade.listEpisodes(comic)
+    if re.match(r"^/user_" + username + "/$", path) and request.method == "PUT":
+        return user_exists(user) or api_facade.createUser(username, params)
+    if re.match(r"^/user_" + username + "/comic_[0-9]+?/read_episodes/$", path) and request.method == "GET":
+        return user_invalid(user, username, request) or comic_missing(comic) or api_facade.listReadEpisodes(user, comic)
+    if re.match(r"^/user_" + username + "/episode_[0-9]+?/$", path) and request.method == "PUT":
+        return user_invalid(user, username, request) or episode_missing(episode) or api_facade.readEpisode(user, episode)
+    if re.match(r"^/user" + username + "/episode_[0-9]+?/$", path) and request.method == "DELETE":
+        return user_invalid(user, username, request) or episode_missing(episode) or api_facade.unreadEpisode(user, episode)
     
 #    if re.match(r"^/user_.+?/$", path) and request.method == "POST":
 #        return user_missing(user) or api_facade.updateUser(user, params)
@@ -57,12 +59,24 @@ def dispatch(request):
 
 # Helper methods for condition checking on the dispatcher
 
-def user_missing(user):
-    """ If the user is missing, returns an error """
-    return HttpResponse("Username not found", status=400) if not user else None 
+def user_invalid(user, username, request):
+    """ Returns an appropriate response if the username does not exist or is not authenticated """
+    # Code heavily inspired on this snippet: http://www.djangosnippets.org/snippets/243/ by Scanner
+    # Credentials must exist, refer to the queried user and be valid
+    if 'HTTP_AUTHORIZATION' in request.META:
+        auth = request.META['HTTP_AUTHORIZATION'].split()
+        if len(auth) == 2:
+            if auth[0].lower() == "basic":
+                uname, passwd = base64.b64decode(auth[1]).split(':')
+                if (uname == username) and user.check_password(passwd):
+                    return None
+    # If no valid credentials were found, ask the user-agent for valid ones                
+    response = HttpResponse("Please supply valid credentials for " + username, status=401)
+    response['WWW-Authenticate'] = 'Basic realm="wcreader"'
+    return response    
 
 def comic_missing(comic):
-    """ If the user is missing, returns an error """
+    """ If the comic is missing, returns an error """
     return HttpResponse("Comic not found", status=400) if not comic else None 
 
 def episode_missing(episode):
